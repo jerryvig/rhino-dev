f552449e585222883800d514bea554b4750c10a6
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-02-13 17:25:29 -0800

    5:30 PM commit.

--- NasdaqProfilesGeocode.js
+++ NasdaqProfilesGeocode.js
@@ -1,12 +1,31 @@
 importPackage(java.io);
+importPackage(java.lang);
 importPackage(Packages.geo.google);
 importPackage(Packages.geo.google.datamodel);
 importPackage(Packages.liquibase.util.csv);
 
 var reader = new CSVReader(new FileReader("/tmp/NasdaqIPOProfiles.csv"));
 var lines = reader.readAll();
+var writer = new CSVWriter(new FileWriter("/tmp/NasdaqIPOProfilesGeocode.csv"));
 reader.close();
 
+var st = new GeoAddressStandardizer( "ABQIAAAAI1oIsi6Dv7MlmxUm1lRR_xTmarcuMJj81CoryY3grjEx5dFcyxQoeQTublWNe-B1iLVnHNrRuJD6_w" );
+
 for ( var i=0; i<lines.size(); i++ ) {
-   print( lines.get(i)[0] );
+   var row = lines.get(i);
+   var addyString = row[11].replace("\n",", ");
+   try {
+     var geoList = st.standardizeToGeoUsAddresses( addyString );
+     var city = geoList.get(0).getCity();
+     var state = geoList.get(0).getState();
+     var outArray = Array();
+     for ( var j=0; j<16; j++ ) outArray[j] = row[j];
+     outArray[16] = city;
+     outArray[17] = state;
+     writer.writeNext( outArray );
+     print( row[1] + ", " + city + ", " + state );
+   } catch ( err ) {}
+   Thread.sleep( 250 );
 }
+
+writer.close();
