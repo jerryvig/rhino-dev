5e5cf06cd80f60b8d593546c1807edd6845b31b9
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-02-19 03:02:32 -0500

    12:05 AM commit

--- ScrapeYahooDividends.js
+++ ScrapeYahooDividends.js
@@ -1,14 +1,16 @@
 importPackage(java.io);
 importPackage(java.util);
 importPackage(java.lang);
+importPackage(java.text);
 importPackage(Packages.liquibase.util.csv);
 importPackage(org.openqa.selenium.htmlunit);
 importPackage(com.gargoylesoftware.htmlunit);
 importPackage(org.openqa.selenium);
 
-var today = new Date();
+var todayString = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
 
 var reader = new CSVReader( new FileReader("/mnt/DriveTwo/rhino-dev/ETFList.csv" ) );
+var writer = new BufferedWriter( new FileWriter("/mnt/DriveTwo/rhino-dev/YahooDividends-"+todayString+".csv") );
 var lines = reader.readAll();
 var tickers = Array();
 
@@ -29,8 +31,14 @@ for ( var i=0; i<tickers.length; i++ ) {
 
 function scrapeTicker( ticker ) {
     var urlString = "http://ichart.finance.yahoo.com/table.csv?s="+ticker+"&a=00&b=29&c=2000&d=01&e=19&f=2012&g=v&ignore=.csv";
-    print( urlString );
     driver.get( urlString );
     var contentString = driver.getPageSource();
-    print( contentString );
+    var lines = contentString.split("\n");
+
+    if ( lines[0].startsWith("Date") ) {
+      for ( var j=1; j<lines.length; j++ ) {
+	 var row = lines[j].split(",");
+         writer.write( "\""+ row[0].trim() + "\",\"" + ticker + "\",\"" + row[1].trim() + "\"\n" );  
+      }       
+    }
 }
