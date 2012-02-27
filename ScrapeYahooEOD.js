5c7c10b8e5cc27c20abbe00457b22bbafc28b747
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-02-27 15:22:59 -0500

    Commit Feb 27, 2012

--- ScrapeYahooEOD.js
+++ ScrapeYahooEOD.js
@@ -7,16 +7,15 @@ importPackage(org.openqa.selenium.htmlunit);
 importPackage(com.gargoylesoftware.htmlunit);
 
 var todayString = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
-var reader = new CSVReader( new FileReader("/mnt/DriveTwo/rhino-dev/ETFList.csv" ) );
+var reader = new CSVReader( new FileReader("/mnt/DriveTwo/rhino-dev/CBOEWeekliesList.csv" ) );
 var writer = new CSVWriter( new FileWriter("/mnt/DriveTwo/rhino-dev/YahooEOD-"+todayString+".csv") );
 var lines = reader.readAll();
 var tickers = Array();
 
 var driver = new HtmlUnitDriver( BrowserVersion.FIREFOX_3_6 );
 
-for ( var i=1; i<lines.size(); i++ ) {
-   var row = lines.get(i);
-   tickers[i] = row[1];  
+for ( var i=0; i<lines.size(); i++ ) {
+   tickers[i] = lines.get(i)[0];  
 }
 
 reader.close();
@@ -24,11 +23,11 @@ reader.close();
 for ( var i=0; i<tickers.length; i++ ) {
    print( "Processing ticker = " + tickers[i] );
    scrapeTicker( tickers[i] ); 
-   Thread.sleep( 500 );
+   Thread.sleep( 400 );
 }
 
 function scrapeTicker( ticker ) {
-    var urlString = "http://ichart.finance.yahoo.com/table.csv?s="+ticker+"&d=1&e=19&f=2012&g=d&a=8&b=1&c=2010&ignore=.csv";
+    var urlString = "http://ichart.finance.yahoo.com/table.csv?s="+ticker+"&d=1&e=24&f=2012&g=d&a=8&b=1&c=2010&ignore=.csv";
     driver.get( urlString );
     var lines = driver.getPageSource().split("\n");
     if ( lines[0].startsWith("Date") ) {
